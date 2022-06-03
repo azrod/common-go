c8f06593c6cdb1aafd5b34d391e1444959bf2f12
Mickael Stanislas <contact@mickael-stanislas.com>
2022-06-03 11:09:54 +0200

    ADD Find func

---
+++ map_test.go
@@ -0,0 +1,23 @@
+package common
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFind(t *testing.T) {
+
+	slice := []string{"a", "b", "c", "d"}
+	val := "c"
+	index, found := Find(slice, val)
+	assert.Equal(t, 2, index)
+	assert.True(t, found)
+
+	sliceNotFound := []string{"a", "b", "c", "d"}
+	valNotFound := "e"
+	indexNotFound, notFound := Find(sliceNotFound, valNotFound)
+	assert.Equal(t, -1, indexNotFound)
+	assert.False(t, notFound)
+
+}
